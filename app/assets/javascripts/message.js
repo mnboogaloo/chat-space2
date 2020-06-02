7f4d44d5503aad6ad8b739414695caf12a03003c
mnboogaloo <boomana1957@gmail.com>
2020-06-02 21:00:20 +0900

    commit 2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,93 +10,42 @@ $(function(){
             ${message.date}
           </div>
         </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
+          <div class="lower-message">
+            <p class="lower-message__content">
             ${message.content}
-          </p>
-          <p class="lower-message__image">
+            </p>
+            <p class="lower-message__image">
             ${image}
-          </p>
-        </div>  
+            </p>
+          </div>  
         </div>`
     return html;
   }
-  // function buildHTML(message) {
-  //   if (message.content && message.image) {
-  //     var html = `<div class="message">` +
-  //       `<div class="upper-message">` +
-  //       `<div class="upper-message__user-name">` +
-  //       message.user_name +
-  //       `</div>` +
-  //       `<div class="upper-message__date">` +
-  //       message.created_at +
-  //       `</div>` +
-  //       `</div>` +
-  //       `<div class="lower-message">` +
-  //       `<p class="lower-message__content">` +
-  //       message.content +
-  //       `</p>` +
-  //       `<img src="` + message.image + `" class="lower-message__image" >` +
-  //       `</div>` +
-  //       `</div>`
-  //   } else if (message.content) {
-  //     var html = `<div class="message">` +
-  //       `<div class="upper-message">` +
-  //       `<div class="upper-message__user-name">` +
-  //       message.user_name +
-  //       `</div>` +
-  //       `<div class="upper-message__date">` +
-  //       message.created_at +
-  //       `</div>` +
-  //       `</div>` +
-  //       `<div class="lower-message">` +
-  //       `<p class="lower-message__content">` +
-  //       message.content +
-  //       `</p>` +
-  //       `</div>` +
-  //       `</div>`
-  //   } else if (message.image) {
-  //     var html = `<div class="message">` +
-  //       `<div class="upper-message">` +
-  //       `<div class="upper-message__user-name">` +
-  //       message.user_name +
-  //       `</div>` +
-  //       `<div class="upper-message__date">` +
-  //       message.created_at +
-  //       `</div>` +
-  //       `</div>` +
-  //       `<div class="lower-message">` +
-  //       `<img src="` + message.image + `" class="lower-message__image" >` +
-  //       `</div>` +
-  //       `</div>`
-  //   };
-  //   return html;
-  // }
-
+  
  $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
   var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.message-list').append(html);
-    $('form')[0].reset();
-    $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
-   })
-  .fail(function(){
-    alert('メッセージ送信に失敗しました');
-  })
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message-list').append(html);
+      $('form')[0].reset();
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('メッセージ送信に失敗しました');
+    })
 
-  .always(function(data){　
-  $(".form__submit").prop('disabled', false);
-  })
+    .always(function(data){　
+    $(".form__submit").prop('disabled', false);
+    })
   })
 });
