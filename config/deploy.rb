3390157ac74ea75c0f3585b481ab09781659af03
mnboogaloo <boomana1957@gmail.com>
2020-06-06 19:59:05 +0900

    commit push

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.14.0'
 set :application, 'chat-space2'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:mnboogaloo/chat-space2.git'
+set :repo_url, 'git@github.com:mnboogaloo/chat-space2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
